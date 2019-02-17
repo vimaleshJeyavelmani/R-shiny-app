b0e665ad61d7b6081dba077cec3a2e6f0c76bba1
vimalesh <vimaleshbe@gmail.com>
2019-02-17 17:04:14 +0530

    test app

--- code/ui.R
+++ code/ui.R
@@ -1,24 +1,5 @@
 library(shiny)
-# vimalesh
-# Define UI for application that plots random distributions 
+
 shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Hello Shiny!"),
-  
-  # Sidebar with a slider input for number of observations
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("obs", 
-                  "Number of observations:", 
-                  min = 1, 
-                  max = 1000, 
-                  value = 500)
-    ),
-    
-    # Show a plot of the generated distribution
-    mainPanel(
-      plotOutput("distPlot")
-    )
-  )
+h1("using text box and check box"),textInput("mystring","wirte here"),checkboxInput("mycheckbox","factor Y")
 ))
