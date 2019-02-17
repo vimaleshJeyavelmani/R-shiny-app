d2f3770c22b28062e5f3f3d7b863240ec4dec856
vimalesh <vimaleshbe@gmail.com>
2019-02-17 17:44:16 +0530

    Update ui.R

--- code/ui.R
+++ code/ui.R
@@ -1,6 +1,7 @@
 library(shiny)
 
 shinyUI(fluidPage(
-h1("Reactive"),sliderInput(inputId="sid",label="slider",value=1,min=1,max=30),plotOutput("plot"),
-mainPanel(tabsetPanel(tabPanel("Tab1","first Tab"),tabPanel("Tab2","second Tab"),tabPanel("Tab3","Third Tab")))
+mainPanel(tabsetPanel(tabPanel("Tab1",(
+h1("Reactive"),sliderInput(inputId="sid",label="slider",value=1,min=1,max=30),plotOutput("plot")
+)),tabPanel("Tab2","second Tab"),tabPanel("Tab3","Third Tab")))
 ))
